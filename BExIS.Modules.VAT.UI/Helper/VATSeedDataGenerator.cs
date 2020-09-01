f24b84db2b9c44d95ab9d487f5f9d88e2b9b0203
David Schöne <david.blaa@googlemail.com>
2020-09-01 13:20:39 +0200

    first import

---
+++ BExIS.Modules.VAT.UI/Helper/VATSeedDataGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BExIS.Modules.VAT.UI.Helpers
+{
+    public class VATSeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
