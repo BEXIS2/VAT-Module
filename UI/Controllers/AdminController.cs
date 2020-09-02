d455a49e73eb1a6d903074bbac0068159dc24c38
David Schöne <david.blaa@googlemail.com>
2020-09-02 08:27:30 +0200

    change path of the ui project

---
+++ UI/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.VAT.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UI/Helper/VATSeedDataGenerator.cs
@@ -0,0 +1,45 @@
+using BExIS.Security.Entities.Objects;
+using BExIS.Security.Services.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BExIS.Modules.VAT.UI.Helpers
+{
+    public class VATSeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+            using (FeatureManager featureManager = new FeatureManager())
+            using (OperationManager operationManager = new OperationManager())
+            {
+                //Features
+                List<Feature> features = featureManager.FeatureRepository.Get().ToList();
+
+                Feature VAT = features.FirstOrDefault(f => f.Name.Equals("VAT"));
+                if (VAT == null) VAT = featureManager.Create("VAT", "Interface to a tool that provides visualize, analyze and transform functions");
+
+                Feature AdminFeature = features.FirstOrDefault(f =>
+                    f.Name.Equals("Admin") &&
+                    f.Parent != null &&
+                    f.Parent.Id.Equals(VAT.Id));
+
+                Feature DisplayFeature = features.FirstOrDefault(f =>
+                    f.Name.Equals("Display") &&
+                    f.Parent != null &&
+                    f.Parent.Id.Equals(VAT.Id));
+
+
+                //Operations
+                operationManager.Create("VAT", "Admin", "*", AdminFeature);
+                operationManager.Create("VAT", "Home", "*", DisplayFeature);
+
+            }
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
